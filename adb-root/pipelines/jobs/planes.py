b5544f68ab80f7609bda8f3314fedb1fdb4a0ddb
anmajuma <51311459+anmajuma@users.noreply.github.com>
2022-03-25 10:30:00 +0530

    Update planes.py

--- adb-root/pipelines/jobs/planes.py
+++ adb-root/pipelines/jobs/planes.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit, col, current_timestamp
 from pipelines.utils import transformations, configmanagement as cm
 
-spark = SparkSession.builder.getOrCreate()
+#spark = SparkSession.builder.getOrCreate()
 
 def extract_Planes(filePath, year):
     return spark.read.format("csv").option("header", "true").load(filePath).filter(col("year") == year)
